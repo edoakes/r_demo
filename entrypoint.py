0e491a4a4bdddd417433764524941825ef7d2cb4
Sasha Sobol <sasha@asobol.com>
2021-09-21 13:32:33 -0700

    entrypoint

---
+++ entrypoint.py
@@ -0,0 +1,13 @@
+import ray
+import time
+
+
+@ray.remote
+def sleep(time_period):
+    time.sleep(time_period)
+    return "done"
+
+
+ray.init("auto")
+sleep_for = 600
+print(ray.get(sleep.remote(sleep_for), timeout=sleep_for + 30))
